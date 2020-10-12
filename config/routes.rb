f5ebbd3b31590dbf2092052781f04a3a621c06a9
varuns <varuns@spritle.com>
2020-10-12 07:38:43 +0400

    edit user account

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :articles
 
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
+
 end
